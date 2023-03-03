8f7591eee3966b6dbf4ae3da30b0114740643b43
KinAxell <kin@axell.tokyo>
2023-03-03 18:27:03 +0900

    finish the props lesson

--- src/index.js
+++ src/index.js
@@ -5,43 +5,44 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 //setup variables
 
-const book1 = {
-    img: "https://images-na.ssl-images-amazon.com/images/I/81bGKUa1e0L._AC_UL300_SR300,200_.jpg",
-    title: "Atomic Habits",
-    author: "James Clear",
-};
-const book2 = {
-    img: "https://images-fe.ssl-images-amazon.com/images/I/71wR8wezE1L._AC_UL600_SR600,400_.jpg",
-    title: "12 Rules For Life",
-    author: "Jordan B Peterson",
-};
-const book3 = {
-    img: "https://images-fe.ssl-images-amazon.com/images/I/71J3+5lrCDL._AC_UL600_SR600,400_.jpg",
-    title: "Psychology of Money",
-    author: "Morgan Housel",
-};
+const books = [
+    {
+        image: "https://images-na.ssl-images-amazon.com/images/I/81bGKUa1e0L._AC_UL300_SR300,200_.jpg",
+        title: "Atomic Habits",
+        author: "James Clear",
+    },
+    {
+        image: "https://images-fe.ssl-images-amazon.com/images/I/71wR8wezE1L._AC_UL600_SR600,400_.jpg",
+        title: "12 Rules For Life",
+        author: "Jordan B Peterson",
+    },
+    {
+        image: "https://images-fe.ssl-images-amazon.com/images/I/71J3+5lrCDL._AC_UL600_SR600,400_.jpg",
+        title: "Psychology of Money",
+        author: "Morgan Housel",
+    },
+];
 
 function BookList() {
     return (
         <section className="booklist">
-            <h1 style={{ textAlign: "center" }}>Keane Putra Setiawan's Library</h1>
-            <Book title={book1.title} author={book1.author} image={book1.img}>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, eveniet!</p>
-            </Book>
-            <Book title={book2.title} author={book2.author} image={book2.img} />
-            <Book title={book3.title} author={book3.author} image={book3.img} />
+            {books.map((book) => {
+                return <Book key={book.image} book={book}></Book>;
+            })}
         </section>
     );
 }
 
 const Book = (props) => {
-    const { title, image, author, children } = props;
+    //attribute, eventHandler
+    // onClick, onMouseOver
+    const { title, image, author } = props.book;
     return (
         <article className="book">
             <h1>{title}</h1>
             <img src={image} alt="" />
             <h4>{author}</h4>
-            {children}
+            <button type="button" onClick={}></button>
         </article>
     );
 };
